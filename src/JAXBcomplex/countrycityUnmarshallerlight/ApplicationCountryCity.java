43342aa2b1f45022ed7e95fad221fd5adf2e095b
senin26 <sergey.ionin.7@gmail.com>
2019-03-25 16:45:56 +0300

    Improved

--- src/JAXBcomplex/countrycityUnmarshallerlight/ApplicationCountryCity.java
+++ src/JAXBcomplex/countrycityUnmarshallerlight/ApplicationCountryCity.java
@@ -16,48 +16,8 @@ import java.util.logging.Logger;
 
 public class ApplicationCountryCity {
 
-    public Country createJavaObjectExample1() {
-        Country country = new Country();
-        appendCountryData(country,"Russia","Moscow", Climate.COLD);
-        try {
-            country.getCities().add(insertCity("Saint-Petersburg", 5000000));
-            country.getCities().add(insertCity("Sochi", 365000));
-        } catch (ParseException exception) {
-            Logger.getLogger(ApplicationCountryCity.class.getName()).
-                    log(Level.ALL, "createJavaObjectExample1 threw ParseException", exception);
-        }
-        return country;
-    }
-
-    public void appendCountryData(Country country, String name, String capital, Climate climate) {
-        country.setName(name);
-        country.setCapital(capital);
-        country.setClimate(climate);
-    }
-
-    public City insertCity(String name, int population) throws ParseException {
-        City city = new City();
-        city.setName(name);
-        city.setPopulation(population);
-        return city;
-    }
-
-    public void marshallExample() {
-        try {
-            JAXBContext context = JAXBContext.newInstance(Country.class);
-            Marshaller marshaller = context.createMarshaller();
-            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
-            marshaller.marshal(createJavaObjectExample1(), System.out);
-        } catch (JAXBException exception) {
-            Logger.getLogger(ApplicationCountryCity.class.getName()).
-                    log(Level.SEVERE, "marshallExample threw JAXBException", exception);
-        }
-    }
-
     public void unmarshallExample() throws JAXBException{
         File file = new File("E:\\Serj\\EPAM\\Drafts\\resources\\init_data.xml");
-        //File file = new File("E:\\Serj\\EPAM\\Drafts\\resources\\init_data.xml");
-        //File file = new File("/init_data.xml");
         BufferedInputStream bis = null;
         StringBuilder xml = new StringBuilder();
         int next;
